9ef2b1d8cc3c2cd69352c2638703b59e72866c4d
Benjamin Sznajder <benjams@il.ibm.com>
2026-01-29 15:44:49 +0200

    commit

---
+++ src/datasets/data_models/dataset_names.py
@@ -0,0 +1,24 @@
+from enum import Enum, unique
+
+
+@unique
+class DatasetName(str, Enum):
+    """
+    Enumeration of available RAG benchmark dataset names.
+
+    This enum defines the supported dataset identifiers used throughout the system.
+    Each value represents a unique dataset that can be loaded and processed.
+    The @unique decorator ensures no duplicate values exist.
+
+    Attributes:
+        AI_ARXIV: ArXiv AI papers dataset identifier.
+
+    Example:
+        >>> dataset = DatasetName.AI_ARXIV
+        >>> print(dataset.value)
+        'ai_arxiv'
+        >>> DatasetName("ai_arxiv")
+        <DatasetName.AI_ARXIV: 'ai_arxiv'>
+    """
+
+    AI_ARXIV = "ai_arxiv"
