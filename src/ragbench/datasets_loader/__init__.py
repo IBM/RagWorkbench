d9230ad8d8fb0ef35f29b653ed28d5159a135a84
Benjamin Sznajder <benjams@il.ibm.com>
2026-02-03 16:23:34 +0200

    add_watsonx

--- src/ragbench/datasets_loader/__init__.py
+++ src/ragbench/datasets_loader/__init__.py
@@ -7,9 +7,11 @@ This module provides data loaders for various RAG benchmark datasets.
 from ragbench.datasets_loader.abstract_data_loader import RagDataLoader
 from ragbench.datasets_loader.bioasq_data_loader import BioasqDataLoader
 from ragbench.datasets_loader.hotpot_qa_data_loader import HotpotQaDataLoader
+from ragbench.datasets_loader.watsonx_data_loader import WatsonxDocsQADataLoader
 
 __all__ = [
     "RagDataLoader",
     "BioasqDataLoader",
     "HotpotQaDataLoader",
+    "WatsonxDocsQADataLoader",
 ]
