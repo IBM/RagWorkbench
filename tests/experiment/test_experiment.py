73f25f5aa2dad483deeb69fa4cf202ed5897824c
Assaf Toledo <assaf.toledo@ibm.com>
2026-02-08 15:36:13 +0200

    updates

--- tests/experiment/test_experiment.py
+++ tests/experiment/test_experiment.py
@@ -1,44 +1,47 @@
-from typing import List
-
-from ragbench.api.experiment import Experiment, IngestPipeline, IngestRuntimeParams, IngestArtifact, \
-    InferencePipeline, InferenceRuntimeParams, InferenceParams, IngestParams
+from ragbench.api.inference import InferencePipeline, InferenceRuntimeParams
+from ragbench.api.ingest import IngestPipeline, IngestRuntimeParams
+from ragbench.api.ingest_artifact import IngestArtifact
 from ragbench.datasets_loader import HotpotQaDataLoader
+from ragbench.datasets_loader.data_models.dataset_names import DatasetName
 from ragbench.datasets_loader.data_models.rag_benchmark import RagBenchmark
 from ragbench.datasets_loader.data_models.rag_corpus import RagCorpus
+from ragbench.experiment import Experiment
 
 
 class EmptyIngestPipeline(IngestPipeline):
-
-    def process(self, rag_corpus: RagCorpus, runtime_params: IngestRuntimeParams) -> List[IngestArtifact]:
+    def process(
+        self,
+        dataset_name: DatasetName,
+        rag_corpus: RagCorpus,
+        runtime_params: IngestRuntimeParams,
+    ) -> list[IngestArtifact]:
         pass
+
 
 class EmptyInferencePipeline(InferencePipeline):
-
-    def process(self, rag_benchmark: RagBenchmark, ingest_artifacts: List[IngestArtifact],
-                runtime_params: InferenceRuntimeParams) -> List[IngestArtifact]:
+    def process(
+        self,
+        dataset_name: DatasetName,
+        rag_benchmark: RagBenchmark,
+        ingest_artifacts: list[IngestArtifact],
+        runtime_params: InferenceRuntimeParams,
+    ):
         pass
 
+
 def test_empty_flow():
-    ingest_params = IngestParams()
-    ingest_pipeline = EmptyIngestPipeline(
-        ingest_params=ingest_params)
-
-    inference_params = InferenceParams()
-    inference_pipeline = EmptyInferencePipeline(
-        inference_params=inference_params
-    )
-
     data_loader = HotpotQaDataLoader()
-
+    ingest_pipeline = EmptyIngestPipeline()
+    inference_pipeline = EmptyInferencePipeline()
     experiment = Experiment(
         data_loader=data_loader,
         ingest_pipeline=ingest_pipeline,
-        inference_pipeline=inference_pipeline)
+        inference_pipeline=inference_pipeline,
+    )
 
     ingest_runtime_params = IngestRuntimeParams()
     inference_runtime_params = InferenceRuntimeParams()
-
     experiment.run(
         ingest_runtime_params=ingest_runtime_params,
-        inference_runtime_params=inference_runtime_params
+        inference_runtime_params=inference_runtime_params,
     )
