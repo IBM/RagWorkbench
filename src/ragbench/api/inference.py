13430ca146e94732cd215e002f6887f084a38879
Assaf Toledo <assaf.toledo@ibm.com>
2026-02-10 11:05:09 +0200

    updates

--- src/ragbench/api/inference.py
+++ src/ragbench/api/inference.py
@@ -2,9 +2,9 @@ from abc import ABC, abstractmethod
 
 from pydantic import BaseModel
 
+from ragbench.api.inference_result import InferenceResult
 from ragbench.api.ingest_artifact import IngestArtifact
-from ragbench.datasets_loader.data_models.rag_benchmark import RagBenchmark
-from ragbench.datasets_loader.dataset_names import DatasetName
+from ragbench.datasets_loader.data_models import RagBenchmarkEntry
 
 
 class InferenceRuntimeParams(BaseModel):
@@ -12,12 +12,17 @@ class InferenceRuntimeParams(BaseModel):
 
 
 class InferencePipeline(ABC):
+
+    def __init__(self):
+        self.ingest_artifacts = None
+
+    def set_ingest_artifacts(self, ingest_artifacts: list[IngestArtifact]):
+        self.ingest_artifacts = ingest_artifacts
+
     @abstractmethod
     def process(
         self,
-        dataset_name: DatasetName,
-        rag_benchmark: RagBenchmark,
-        ingest_artifacts: list[IngestArtifact],
+        benchmark_entry: RagBenchmarkEntry,
         runtime_params: InferenceRuntimeParams,
-    ):
+    ) -> InferenceResult:
         pass
---
+++ src/ragbench/api/inference_result.py
@@ -0,0 +1,5 @@
+from ragbench.datasets_loader.data_models import RagBenchmarkEntry
+
+
+class InferenceResult(RagBenchmarkEntry):
+    answer: str
