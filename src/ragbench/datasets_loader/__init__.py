5ab579aa66d73f4b04117665b910b11280e6c88d
Benjamin Sznajder <benjams@il.ibm.com>
2026-02-11 17:26:39 +0200

    commit

--- src/ragbench/datasets_loader/__init__.py
+++ src/ragbench/datasets_loader/__init__.py
@@ -1,6 +1,24 @@
 """
 RAG Benchmark Dataset Loaders.
 
+This module provides data loaders for various RAG benchmark datasets, along with
+a factory class for creating loader instances. It also supports registration of
+custom data loaders for external datasets.
+
+Example:
+    Using built-in datasets:
+    >>> from ragbench.datasets_loader import DataLoaderFactory, DatasetName
+    >>> loader = DataLoaderFactory.create_loader(DatasetName.BIOASQ, split="train")
+    >>> corpus = loader.get_corpus()
+    >>> benchmark = loader.get_benchmark()
+
+    Registering and using custom datasets:
+    >>> from ragbench.datasets_loader import DataLoaderFactory, RagDataLoader
+    >>> class MyLoader(RagDataLoader):
+    ...     def _get_documents(self): return [...]
+    ...     def _get_benchmark_entries(self, split): return [...]
+    >>> DataLoaderFactory.register_loader("my_dataset", MyLoader)
+    >>> loader = DataLoaderFactory.create_loader("my_dataset", split="train")
 """
 
 from ragbench.datasets_loader.abstract_data_loader import RagDataLoader
