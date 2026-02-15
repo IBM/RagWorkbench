3ddc312d9b4186adf2906a6875fbadf938efc8f9
Assaf Toledo <assaf.toledo@ibm.com>
2026-02-15 13:53:31 +0200

    updates

---
+++ src/ragbench/boards/board_registry_static.py
@@ -0,0 +1,53 @@
+from typing import Any, TypeVar
+
+from ragbench.api.inference import InferenceParams, InferencePipeline
+from ragbench.api.ingest import IngestParams, IngestPipeline
+
+INGEST_T1 = TypeVar("INGEST_T1", bound=IngestPipeline)
+INGEST_T2 = TypeVar("INGEST_T2", bound=IngestParams)
+
+INFERENCE_T1 = TypeVar("INFERENCE_T1", bound=InferencePipeline)
+INFERENCE_T2 = TypeVar("INFERENCE_T2", bound=InferenceParams)
+
+
+class BoardRegistryStatic:
+    _ingest_pipelines: dict[str, tuple[type[IngestPipeline], type[IngestParams]]] = {}
+    _inference_pipelines: dict[
+        str, tuple[type[InferencePipeline], type[InferenceParams]]
+    ] = {}
+
+    @classmethod
+    def register_ingest(
+        cls,
+        name: str,
+        ingest_class: type[INGEST_T1],
+        params_class: type[INGEST_T2],
+    ):
+        cls._ingest_pipelines[name] = (ingest_class, params_class)
+
+    @classmethod
+    def register_inference(
+        cls,
+        name: str,
+        inference_class: type[INFERENCE_T1],
+        params_class: type[INFERENCE_T2],
+    ):
+        cls._inference_pipelines[name] = (inference_class, params_class)
+
+    @classmethod
+    def create_ingest_pipeline(
+        cls,
+        name: str,
+        params: dict[str, Any],
+    ):
+        ingest_class, params_class = cls._ingest_pipelines[name]
+        return ingest_class(params_class(**params))
+
+    @classmethod
+    def create_inference_pipeline(
+        cls,
+        name: str,
+        params: dict[str, Any],
+    ):
+        inference_class, inference_params = cls._inference_pipelines[name]
+        return inference_class(inference_params(**params))
