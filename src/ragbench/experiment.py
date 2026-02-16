50a1afc699d121188fec898a7cd45d50cb10dfa0
Benjamin Sznajder <benjams@il.ibm.com>
2026-02-16 16:44:58 +0200

    add_todo

--- src/ragbench/experiment.py
+++ src/ragbench/experiment.py
@@ -29,77 +29,85 @@ class Experiment:
 
         # prepare the data
         rag_benchmark = self.data_loader.get_benchmark()
-        rag_benchmark_df = pd.DataFrame(
-            entry.model_dump() for entry in rag_benchmark.get_benchmark_entries()
+        # rag_benchmark_df = pd.DataFrame(
+        #     entry.model_dump() for entry in rag_benchmark.get_benchmark_entries()
+        # )
+        # dataset = mlflow.data.from_pandas(
+        #     rag_benchmark_df,
+        #     source="my source",
+        #     name=self.data_loader.dataset_name,
+        #     targets=None,
+        # )
+
+        # with mlflow.start_run():
+        #     mlflow.log_param("experiment.name", self.name)
+        #     mlflow.log_param("experiment.id", experiment_id)
+        #
+        #     # log the dataset
+        #     mlflow.log_input(dataset, context="inference")
+        #
+        #     # log the dataset properties
+        #     mlflow.log_param("dataset.name", self.data_loader.dataset_name)
+        #     mlflow.log_param("dataset.split", self.data_loader.split)
+        #     mlflow.log_param(
+        #         "dataset.question_limit",
+        #         self.data_loader.sampling_params.question_limit,
+        #     )
+        #     mlflow.log_param(
+        #         "dataset.document_factor",
+        #         self.data_loader.sampling_params.document_factor,
+        #     )
+
+        # run the ingest
+        ingest_artifacts = self.ingest_pipeline.process(
+            data_loader=self.data_loader
         )
-        dataset = mlflow.data.from_pandas(
-            rag_benchmark_df,
-            source="my source",
-            name=self.data_loader.dataset_name,
-            targets=None,
+
+        # set the ingest artifacts for the inference pipeline
+        self.inference_pipeline.set_ingest_artifacts(
+            ingest_artifacts=ingest_artifacts
         )
 
-        with mlflow.start_run():
-            mlflow.log_param("experiment.name", self.name)
-            mlflow.log_param("experiment.id", experiment_id)
+        # ml-flow wrapper for the inference part
+        # def model_fn(benchmark_df: pd.DataFrame) -> pd.DataFrame:
+        results = []
 
-            # log the dataset
-            mlflow.log_input(dataset, context="inference")
+        # iterate the benchmark dataframe
+        for entry in rag_benchmark.get_benchmark_entries():
+            # back to python classes
+            # entry = RagBenchmarkEntry.model_validate(row)
 
-            # log the dataset properties
-            mlflow.log_param("dataset.name", self.data_loader.dataset_name)
-            mlflow.log_param("dataset.split", self.data_loader.split)
-            mlflow.log_param(
-                "dataset.question_limit",
-                self.data_loader.sampling_params.question_limit,
-            )
-            mlflow.log_param(
-                "dataset.document_factor",
-                self.data_loader.sampling_params.document_factor,
-            )
+            # run the inference
+            result = self.inference_pipeline.process(benchmark_entry=entry)
 
-            # run the ingest
-            ingest_artifacts = self.ingest_pipeline.process(
-                data_loader=self.data_loader
-            )
+            # collect the result
+            results.append(result.model_dump())
+        # Results contain a list of inference_results
 
-            # set the ingest artifacts for the inference pipeline
-            self.inference_pipeline.set_ingest_artifacts(
-                ingest_artifacts=ingest_artifacts
-            )
+        # TODO : We must apply the metrics
+        # Look at evaluator.py from RagWorkbench - we want to run unitxt
+        # Look at board_model.py from RagWorkbench -
 
-            # ml-flow wrapper for the inference part
-            def model_fn(benchmark_df: pd.DataFrame) -> pd.DataFrame:
-                results = []
+        # We can return a kind of PatternResult class
 
-                # iterate the benchmark dataframe
-                for _row_id, row in benchmark_df.iterrows():
-                    # back to python classes
-                    entry = RagBenchmarkEntry.model_validate(row)
 
-                    # run the inference
-                    result = self.inference_pipeline.process(benchmark_entry=entry)
+        # return a new dataframe
+        return pd.DataFrame(results)
 
-                    # collect the result
-                    results.append(result.model_dump())
+            # # ml-flow wrapper for evaluation
+            # def multi_task_evaluator(eval_df, predictions, targets):
+            #     return {
+            #         "mae_word_count": 0,
+            #         "sentiment_accuracy": 0,
+            #         "sentence_split_accuracy": 0,
+            #     }
+            #
+            # mlflow.evaluate(
+            #     model=model_fn,
+            #     data=rag_benchmark_df,
+            #     targets=None,  # we have multiple targets → so we handle them in evaluator
+            #     model_type="text",
+            #     evaluators=[multi_task_evaluator],
+            # )
 
-                # return a new dataframe
-                return pd.DataFrame(results)
-
-            # ml-flow wrapper for evaluation
-            def multi_task_evaluator(eval_df, predictions, targets):
-                return {
-                    "mae_word_count": 0,
-                    "sentiment_accuracy": 0,
-                    "sentence_split_accuracy": 0,
-                }
-
-            mlflow.evaluate(
-                model=model_fn,
-                data=rag_benchmark_df,
-                targets=None,  # we have multiple targets → so we handle them in evaluator
-                model_type="text",
-                evaluators=[multi_task_evaluator],
-            )
-
-            print("Run:", mlflow.active_run().info.run_id)
+            # print("Run:", mlflow.active_run().info.run_id)
