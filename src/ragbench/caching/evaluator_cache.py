4e3d1b0f19b66604afa7d4b136312fe36b93483b
Benjamin Sznajder <benjams@il.ibm.com>
2026-02-24 11:34:40 +0200

    add_evaluation

---
+++ src/ragbench/caching/evaluator_cache.py
@@ -0,0 +1,68 @@
+import json
+from pathlib import Path
+from typing import Any
+
+from ragbench.caching.abstract_file_system_cache import AbstractFileSystemCache
+
+
+class EvaluatorCache(AbstractFileSystemCache):
+    def __init__(
+        self,
+        cache_dir: Path | str,
+        config_params: dict,
+        cache_key_fields: set | None = None,
+    ):
+        super().__init__(cache_dir, cache_name="evaluator", config_dict=config_params)
+        self.cache_key_fields = cache_key_fields
+        if self.cache_key_fields is None or len(self.cache_key_fields) == 0:
+            # By default, we look at all the fields...
+            self.cache_key_fields = {
+                "question",
+                "answer",
+                "ground_truths",
+                "ground_truths_context_ids",
+                "context_ids",
+                "contexts",
+            }
+
+    def _get_parameters_hash(self, reduced_dict: dict[str, Any]) -> str:
+        return AbstractFileSystemCache.get_hash_dict(reduced_dict)
+
+    def _read_content(self, file: Path) -> Any:
+        return json.loads(file.read_text(encoding="utf-8"))
+
+    def _content_to_json(self, scores_dict: dict[str, float]) -> str:
+        return json.dumps(scores_dict, indent=4)
+
+    def _create_key_dict(self, evaluation_dict: dict[str, Any]) -> dict[str, Any]:
+        assert self.cache_key_fields is not None, "cache_key_fields is None"
+        missing_keys = [
+            key for key in self.cache_key_fields if key not in evaluation_dict.keys()
+        ]
+        if missing_keys:
+            raise RuntimeError(
+                f"Missing fields in evaluation dict: missing '{missing_keys}', expected '{self.cache_key_fields}', existing: {evaluation_dict.keys()}."
+            )
+        reduced_dict = {
+            k: v for k, v in evaluation_dict.items() if k in self.cache_key_fields
+        }
+
+        return reduced_dict
+
+    # We force signature
+    def add(self, evaluation_dict: dict[str, Any], score_dict: dict[str, float]):
+        key_dict = self._create_key_dict(evaluation_dict)
+        # For debugging, we keep also the parameters that generated the entry in the cache:
+        cache_value = key_dict | score_dict
+        super().add(key_dict, cache_value)
+
+    # We force signature
+    def get(self, evaluation_dict: dict[str, Any]):
+        key_dict = self._create_key_dict(evaluation_dict)
+        cached_value, _ = super()._get(key_dict)
+        # We remove the key_dict for the cached returned values:
+        if cached_value:
+            cached_value = {
+                k: v for k, v in cached_value.items() if k not in key_dict.keys()
+            }
+        return cached_value
---
+++ src/ragbench/eval/evaluation.py
@@ -0,0 +1,87 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+import numpy as np
+from scipy.stats import bootstrap  # type: ignore[import-not-found]
+
+
+class EvaluationException(Exception):
+    """
+    Raised for eval errors of mis-configurations.
+    """
+
+
+class BaseEvaluationMetric(ABC):
+    """
+    This class defines the functionality to evaluate a RAG application
+    and compare different RAG applications.
+    """
+
+    def __init__(
+        self,
+        name: str,
+        metric_params: dict[str, Any],
+        **kwargs,
+    ):
+        self.name = name
+        self.metric_params = metric_params
+
+    @abstractmethod
+    def compute(self, dataset: list[dict[str, Any]]):
+        """
+        Evaluate the RAGPattern.query() response against list of different metrics.
+
+        Parameters
+        ----------
+        dataset : List[Dict[str, Any]]
+            Dataset to preform the metric eval on.
+
+        Returns
+        -------
+        dict
+            Evaluation result data
+        """
+
+    @abstractmethod
+    def get_name(self) -> str:
+        """
+        Returns the metric full name
+
+        Returns
+        -------
+        str
+            the full name of the metric
+        """
+
+    @abstractmethod
+    def get_score_names(self) -> list[str]:
+        pass
+
+    def full_score_name(self, score_name):
+        return (
+            self.name
+            if score_name == self.name
+            else self.name[: self.name.rindex(".")] + "." + score_name
+        )
+
+    @staticmethod
+    def compute_stats(scores: list[float]) -> dict[str, float]:
+        full_size = len(scores)
+        scores = np.array(scores)  # type: ignore[assignment]
+        scores = scores[np.isfinite(scores)]  # type: ignore[assignment]
+        coverage = len(scores) / full_size
+        mean = np.mean(scores)
+        if np.allclose(scores, mean):
+            ci = mean, mean
+        else:
+            ci = bootstrap((scores,), np.mean, random_state=1024).confidence_interval
+        return {
+            "mean": float(mean),
+            "ci_low": float(ci[0]),
+            "ci_high": float(ci[1]),
+            "coverage": coverage,
+        }
+
+    @abstractmethod
+    def cleanup(self) -> None:
+        pass
