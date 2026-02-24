0fd599860e9e74234c3a3459831e41449512a19a
Benjamin Sznajder <benjams@il.ibm.com>
2026-02-24 12:51:09 +0200

    add_evaluatio_todo

--- src/ragbench/experiment.py
+++ src/ragbench/experiment.py
@@ -1,12 +1,8 @@
-import uuid
-
-import mlflow
-import pandas as pd
-
 from ragbench.api.inference import InferencePipeline
+from ragbench.api.inference_result import InferenceResult
 from ragbench.api.ingest import IngestPipeline
 from ragbench.datasets_loader import RagDataLoader
-from ragbench.datasets_loader.data_models import RagBenchmarkEntry
+from ragbench.datasets_loader.data_models import RagBenchmark
 
 
 class Experiment:
@@ -23,82 +19,25 @@ class Experiment:
         self.inference_pipeline = inference_pipeline
 
     def run(self):
-        experiment_id = f"{self.name}/{str(uuid.uuid4())}"
-        mlflow.set_tracking_uri("sqlite:///mlflow.db")
-        mlflow.set_experiment(experiment_id)
 
         # prepare the data
-        rag_benchmark = self.data_loader.get_benchmark()
-        rag_benchmark_df = pd.DataFrame(
-            entry.model_dump() for entry in rag_benchmark.get_benchmark_entries()
-        )
-        dataset = mlflow.data.from_pandas(
-            rag_benchmark_df,
-            source="my source",
-            name=self.data_loader.dataset_name,
-            targets=None,
-        )
+        rag_benchmark: RagBenchmark = self.data_loader.get_benchmark()
 
-        with mlflow.start_run():
-            mlflow.log_param("experiment.name", self.name)
-            mlflow.log_param("experiment.id", experiment_id)
+        # run the ingest
+        ingest_artifacts = self.ingest_pipeline.process(data_loader=self.data_loader)
 
-            # log the dataset
-            mlflow.log_input(dataset, context="inference")
+        # set the ingest artifacts for the inference pipeline
+        self.inference_pipeline.set_ingest_artifacts(ingest_artifacts=ingest_artifacts)
 
-            # log the dataset properties
-            mlflow.log_param("dataset.name", self.data_loader.dataset_name)
-            mlflow.log_param("dataset.split", self.data_loader.split)
-            mlflow.log_param(
-                "dataset.question_limit",
-                self.data_loader.sampling_params.question_limit,
+        # ml-flow wrapper for the inference part
+        results: list[InferenceResult] = []
+        for benchmark_entry in rag_benchmark.get_benchmark_entries():
+            # run the inference
+            result: InferenceResult = self.inference_pipeline.process(
+                benchmark_entry=benchmark_entry
             )
-            mlflow.log_param(
-                "dataset.document_factor",
-                self.data_loader.sampling_params.document_factor,
-            )
+            # collect the result
+            results.append(result)
 
-            # run the ingest
-            ingest_artifacts = self.ingest_pipeline.process(
-                data_loader=self.data_loader
-            )
-
-            # set the ingest artifacts for the inference pipeline
-            self.inference_pipeline.set_ingest_artifacts(
-                ingest_artifacts=ingest_artifacts
-            )
-
-            # ml-flow wrapper for the inference part
-            def model_fn(benchmark_df: pd.DataFrame) -> pd.DataFrame:
-                results = []
-
-                # iterate the benchmark dataframe
-                for _row_id, row in benchmark_df.iterrows():
-                    # back to python classes
-                    entry = RagBenchmarkEntry.model_validate(row)
-
-                    # run the inference
-                    result = self.inference_pipeline.process(benchmark_entry=entry)
-
-                    # collect the result
-                    results.append(result.model_dump())
-
-                # return a new dataframe
-                return pd.DataFrame(results)
-
-            # ml-flow wrapper for eval
-            def multi_task_evaluator(eval_df, predictions, targets):
-                return {
-                    "mae_word_count": 0,
-                    "sentiment_accuracy": 0,
-                    "sentence_split_accuracy": 0,
-                }
-
-            mlflow.evaluate(
-                model=model_fn,
-                data=rag_benchmark_df,
-                targets=None,  # we have multiple targets → so we handle them in evaluator
-                evaluators=[multi_task_evaluator],
-            )
-
-            print("Run:", mlflow.active_run().info.run_id)
+        # Now run the evaluation via the evaluator code!
+        # TODO!
