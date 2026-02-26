301722bdf4b3d08424fded0a46cfd7039747032f
Benjamin Sznajder <benjams@il.ibm.com>
2026-02-26 10:41:46 +0200

    add_context_ids

--- src/ragbench/api/inference_result.py
+++ src/ragbench/api/inference_result.py
@@ -3,6 +3,5 @@ from ragbench.datasets_loader.data_models import RagBenchmarkEntry
 
 class InferenceResult(RagBenchmarkEntry):
     answer: str
-    # TODO - Add
-    # "context_ids",
-    # "contexts",
+    context_ids: list[str] | None = None
+    contexts: list[str] | None = None
